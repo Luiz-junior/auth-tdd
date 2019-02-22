689eff290aaddd29b3edc06ea32b53bc1041ca87
luiz-junior <junior.rodrigues0222@gmail.com>
2019-02-22 13:23:25 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: '',
+  password: '',
+  database: '',
+  dialect: 'postgres',
+  operatorAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
